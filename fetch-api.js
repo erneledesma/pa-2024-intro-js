6b9cfb90b1b0cf5a8c2b1f0ce7d95612f61dd928
Ernesto Ledesma <eeledesma@teco.com.ar>
2024-08-19 19:54:23 -0300

    [add]:final de fetch-api

--- fetch-api.js
+++ fetch-api.js
@@ -7,11 +7,10 @@ const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey}`
 peticion
     .then( resp => resp.json() )
     .then(({ data })  => {
-        console.log(data.images.original.url)
-
+       const {url} = data.images.original
+        console.log(data)
         const img = document.createElement('img')
         img.src = url
-
-        document.body.appendChild(img)
+        document.body.append(img)
     })
     .catch( console.warn)
